e5cc1e75c6888d2f63939e0a575d53e994839785
mkelemana91 <42268009+mkelemana91@users.noreply.github.com>
2020-07-07 22:08:14 +0200

    Create datagen.sh

---
+++ datagen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SED_FILE=data
+
+TEMPLATE_FILE=$1
+TARGET_FILE=$2
+DB_SOURCE_VALUE=$3
+DB_USER_VALUE=$4
+DB_PASSWORD_VALUE=$5
+ENV_VALUE=$6
+PREMISE_ID_VALUE=$7
+LEARNER_COUNT_VALUE=$8
+
+printf 's:$DBSource:'"$DB_SOURCE_VALUE"':\n' > $SED_FILE
+printf 's:$DBUser:'"$DB_USER_VALUE"':\n' >> $SED_FILE
+printf 's:$DBPassword:'"$DB_PASSWORD_VALUE"':\n' >> $SED_FILE
+printf 's:$Env:'"$ENV_VALUE"':\n' >> $SED_FILE
+printf 's:$PremiseId:'"$PREMISE_ID_VALUE"':\n' >> $SED_FILE
+printf 's:$LearnerCount:'"$LEARNER_COUNT_VALUE"':\n' >> $SED_FILE
+printf 's:$CourseId:'"$COURSE_ID_VALUE"':\n' >> $SED_FILE
+
+sed -f $SED_FILE $TEMPLATE_FILE > $TARGET_FILE
+
+if [ $? -eq 0 ]; then
+   echo sed replacement command passed!
+   chmod +x ./mm.data.generator.exe
+   ./mm.data.generator.exe
+else
+   oops! something went wrong!
+fi
